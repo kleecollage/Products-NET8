44311b9e91be13b64cac541376fb5f4a466bf0b7
Klee Collage <klee.collage@gmail.com>
2025-02-20 16:41:05 -0600

    feat(ThemesLisr): Implement view with table that shows all the movie themes

--- Controllers/RepositoryController.cs
+++ Controllers/RepositoryController.cs
@@ -1,19 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Web.Data;
+using Web.Repository;
 
 namespace Web.Controllers;
 
-public class RepositoryController(IWebHostEnvironment hostingEnvironmet): Controller
+public class RepositoryController: Controller
 {
-  private readonly IWebHostEnvironment _hostingEnvironmet = hostingEnvironmet;
+  private readonly IWebHostEnvironment _hostingEnvironmet;
+  private readonly TematicaRepository _tematicaRepository;
+  [TempData] public string FlashClass { get; set; }
+  [TempData] public string FlashMessage { get; set; }
 
-  [TempData]
-  public string FlashClass { get; set; }
-
-  [TempData]
-  public string FlashMessage { get; set; }
+  public RepositoryController(ApplicationDbContext context, IWebHostEnvironment hostingEnvironmet)
+  {
+    _hostingEnvironmet = hostingEnvironmet;
+    _tematicaRepository = new TematicaRepository(context);
+  }
 
   [Route("/repository-pattern")]
   public IActionResult Index()
+  {
+    return View();
+  }
+
+  [Route("/repository-pattern/themes")]
+  public IActionResult ThemesList()
+  {
+    var data = _tematicaRepository.GetAll();
+    return View(data);
+  }
+
+  [Route("/repository-pattern/themes/add")]
+  public IActionResult ThemeAdd()
   {
     return View();
   }
